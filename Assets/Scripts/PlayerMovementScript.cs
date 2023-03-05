c216aa491bae874b288f62c5d409bce1b7cfa96b
jmazurk <kuba.mazurkiewicz05@gmail.com>
2023-03-05 21:24:27 +0100

    Initial commit

---
+++ Assets/Scripts/PlayerMovementScript.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementScript : MonoBehaviour
+{
+    private Rigidbody2D myRigidBody;
+    void Awake()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
