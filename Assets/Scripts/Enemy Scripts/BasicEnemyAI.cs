fff6c39fb07571b1a6946ea32bf0d0279d7d6891
jmazurk <kuba.mazurkiewicz05@gmail.com>
2023-06-01 22:25:56 +0200

    Testing

--- Assets/Scripts/Enemy Scripts/BasicEnemyAI.cs
+++ Assets/Scripts/Enemy Scripts/BasicEnemyAI.cs
@@ -12,7 +12,7 @@ public class BasicEnemyAI : MonoBehaviour
         myStats = GetComponent<CreatureStats>();
         dataManager = GameObject.FindGameObjectWithTag("data_manager").GetComponent<Data>();
         playerStats = dataManager.playerStats;
-        //test pull request
+        //test pull request2
     }
 
     
